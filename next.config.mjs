0bdd2388051103f556fc9974b7920d66d5df415e
Daniel Dogeanu <contact@danieldogeanu.com>
2024-03-09 05:01:15 +0200

    install sass

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
+import path from 'node:path';
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  sassOptions: {
+    includePaths: [path.join(process.cwd(), 'src', 'styles')],
+  },
+};
 
 export default nextConfig;

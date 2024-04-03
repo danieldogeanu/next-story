7f79441ab0b6c77460862f2e1ca4b9e139a7c2c4
Daniel Dogeanu <contact@danieldogeanu.com>
2024-04-03 14:51:33 +0300

    add submenu to nav-entry

--- frontend/src/components/nav-entry.tsx
+++ frontend/src/components/nav-entry.tsx
@@ -1,24 +1,70 @@
 'use client';
 
 import Link from 'next/link';
-import { Button } from '@mantine/core';
-import { usePathname } from 'next/navigation';
+import { Button, Menu } from '@mantine/core';
+import { usePathname, useRouter } from 'next/navigation';
+import { IconChevronDown, IconChevronUp } from '@tabler/icons-react';
+import { useState } from 'react';
 
 export interface NavEntryProps {
   href: string;
-  children: React.ReactNode;
+  label: string;
+  submenu?: NavEntryProps[];
 }
 
-export default function NavEntry({href, children}: NavEntryProps) {
+export default function NavEntry({href, label, submenu}: NavEntryProps) {
   const pathname = usePathname();
+  const router = useRouter();
   const isActive = () => (pathname === href);
+  const hasSubmenu = () => (submenu !== undefined && submenu.length > 0);
+  const [submenuOpened, setSubmenuOpened] = useState(false);
 
   return (
-    <Button
-      component={Link}
-      href={href}
-      color='dark'
-      variant={isActive() ? 'light' : 'subtle'}
-    >{children}</Button>
+    <>
+      {hasSubmenu() ?
+        <Menu
+          trigger='click'
+          shadow='md'
+          width={200}
+          loop={false}
+          withinPortal={false}
+          trapFocus={false}
+          menuItemTabIndex={0}
+          opened={submenuOpened}
+          onChange={setSubmenuOpened}
+        >
+          <Menu.Target>
+            <Button
+              component={Link}
+              href={href}
+              color='dark'
+              variant={isActive() ? 'light' : 'subtle'}
+              rightSection={submenuOpened ? <IconChevronUp /> : <IconChevronDown />}
+              onClick={(e) => {
+                if (submenuOpened) router.push(href);
+                else e.preventDefault();
+              }}
+            >{label}</Button>
+          </Menu.Target>
+
+          <Menu.Dropdown>
+            {submenu && submenu.map((item, index) => (
+              <Menu.Item 
+                key={index}
+                component={Link}
+                href={item.href}
+              >{item.label}</Menu.Item>
+            ))}
+          </Menu.Dropdown>
+        </Menu>
+        :
+        <Button
+          component={Link}
+          href={href}
+          color='dark'
+          variant={isActive() ? 'light' : 'subtle'}
+        >{label}</Button>
+      }
+    </>
   );
 }
--- frontend/src/components/sitenav.tsx
+++ frontend/src/components/sitenav.tsx
@@ -1,15 +1,27 @@
 'use server';
 
-import NavEntry from '@/components/nav-entry';
+import NavEntry, { NavEntryProps } from '@/components/nav-entry';
 import styles from '@/styles/sitenav.module.scss';
 
 export default async function SiteNav() {
+  const navEntries: NavEntryProps[] = [
+    { href: '/', label: 'Home' },
+    { href: '/about', label: 'About' },
+    { href: '/categories', label: 'Categories', submenu: [
+      { href: '/categories/first', label: 'First' },
+      { href: '/categories/second', label: 'Second' },
+      { href: '/categories/third', label: 'Third' },
+      { href: '/categories/fourth', label: 'Fourth' },
+      { href: '/categories/fifth', label: 'Fifth' },
+    ] },
+    { href: '/contact', label: 'Contact' },
+  ];
+
   return (
     <nav className={styles.container}>
-      <NavEntry href='/'>Home</NavEntry>
-      <NavEntry href='/about'>About</NavEntry>
-      <NavEntry href='/services'>Services</NavEntry>
-      <NavEntry href='/contact'>Contact</NavEntry>
+      {navEntries && navEntries.map((entry, index) => (
+        <NavEntry key={index} {...entry} />
+      ))}
     </nav>
   );
 }

5cd0658bc0348dc642dc26126d765166079376a4
Daniel Dogeanu <contact@danieldogeanu.com>
2024-04-05 08:24:06 +0300

    refactor social nav to iterate trough data

--- frontend/src/components/socialnav.tsx
+++ frontend/src/components/socialnav.tsx
@@ -1,16 +1,22 @@
 'use client';
 
-import SocialIcon from '@/components/social-icon';
+import SocialIcon, { SocialIconProps } from '@/components/social-icon';
 import { IconBrandDribbble, IconBrandGithub, IconBrandInstagram, IconBrandLinkedin } from '@tabler/icons-react';
 import styles from '@/styles/socialnav.module.scss';
 
 export default function SocialNav() {
+  const socialIcons: SocialIconProps[] = [
+    { icon: IconBrandLinkedin, label: 'LinkedIn', href: 'https://linkedin.com/in/danieldogeanu' },
+    { icon: IconBrandGithub, label: 'GitHub', href: 'https://github.com/danieldogeanu' },
+    { icon: IconBrandDribbble, label: 'Dribbble', href: 'https://dribbble.com/danieldogeanu' },
+    { icon: IconBrandInstagram, label: 'Instagram', href: 'https://instagram.com/danieldogeanu' },
+  ];
+
   return (
     <nav className={styles.container}>
-      <SocialIcon icon={IconBrandLinkedin} label='LinkedIn' href='https://linkedin.com/in/danieldogeanu' />
-      <SocialIcon icon={IconBrandGithub} label='GitHub' href='https://github.com/danieldogeanu' />
-      <SocialIcon icon={IconBrandDribbble} label='Dribbble' href='https://dribbble.com/danieldogeanu' />
-      <SocialIcon icon={IconBrandInstagram} label='Instagram' href='https://instagram.com/danieldogeanu' />
+      {socialIcons && socialIcons.map((socialIcon, index) => (
+        <SocialIcon key={index} icon={socialIcon.icon} label={socialIcon.label} href={socialIcon.href} />
+      ))}
     </nav>
   );
 }

412f6e2d1eb247fe6f152ce6c41e82a8e2b00555
Daniel Dogeanu <contact@danieldogeanu.com>
2024-04-06 07:51:11 +0300

    add forward ref to action entry

--- frontend/src/components/action-entry.tsx
+++ frontend/src/components/action-entry.tsx
@@ -1,25 +1,32 @@
 'use client';
 
 import { ActionIcon, ActionIconProps, ElementProps } from '@mantine/core';
+import { ForwardedRef, forwardRef } from 'react';
 import { useViewportSize } from '@mantine/hooks';
 import { TablerIconsProps } from '@tabler/icons-react';
 
-export interface ActionEntryProps extends ActionIconProps, ElementProps<'button', keyof ActionIconProps> {
+export interface ActionEntryProps extends 
+  ActionIconProps, ElementProps<'button', keyof ActionIconProps> {
   icon: React.FC<TablerIconsProps>;
   label: string;
 }
 
-export default function ActionEntry({icon, label, ...other}: ActionEntryProps) {
+const ActionEntry = forwardRef(function ActionEntry(
+  {icon, label, ...other}: ActionEntryProps,
+  ref: ForwardedRef<HTMLButtonElement>
+) {
   const {width} = useViewportSize();
   const TablerIcon = icon;
-
+  
   return (
     <ActionIcon 
       variant='subtle' color='dark' 
       size={(width > 360) ? 'xl' : 'lg'}
       aria-label={label} title={label}
-      {...other}>
+      {...other} ref={ref}>
       <TablerIcon stroke={2} size={24} />
     </ActionIcon>
   );
-}
+});
+
+export default ActionEntry;

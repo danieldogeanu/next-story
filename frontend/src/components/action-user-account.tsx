ad5cf0476fe2b2dd935fca83208251840da9fff8
Daniel Dogeanu <contact@danieldogeanu.com>
2024-04-08 10:04:36 +0300

    add tabs & forms to the user account action

--- frontend/src/components/action-user-account.tsx
+++ frontend/src/components/action-user-account.tsx
@@ -1,12 +1,12 @@
 'use client';
 
-import { Drawer, ScrollArea, Tabs } from '@mantine/core';
+import { Drawer, ScrollArea, Stack, Tabs } from '@mantine/core';
+import { useDisclosure } from '@mantine/hooks';
 import { IconUser } from '@tabler/icons-react';
 import ActionEntry from '@/components/action-entry';
 import LoginForm from '@/components/login-form';
 import RegisterForm from '@/components/register-form';
 import styles from '@/styles/action-user-account.module.scss';
-import { useDisclosure } from '@mantine/hooks';
 
 // TODO: Handle authentication and replace login/register forms with submenu if authenticated.
 // TODO: Replace user account icon with profile avatar when authenticated.
@@ -27,7 +27,7 @@ export default function ActionUserAccount({...props}) {
       <Drawer
         title='User Account'
         position='right'
-        size='30%'
+        size={400}
         className={styles.drawer}
         opened={opened}
         onClose={close}
@@ -37,8 +37,34 @@ export default function ActionUserAccount({...props}) {
           transition: 'slide-left',
           timingFunction: 'ease',
           duration: 150,
-        }}
-      >
+        }}>
+
+        <Stack className={styles.drawerContent}>
+
+          <Tabs
+            className={styles.tabsContent}
+            variant='pills' defaultValue='login'>
+
+            <Tabs.List grow>
+              <Tabs.Tab size='md' value='login'>
+                Login
+              </Tabs.Tab>
+              <Tabs.Tab size='md' value='register'>
+                Register
+              </Tabs.Tab>
+            </Tabs.List>
+
+            <Tabs.Panel value='login'>
+              <LoginForm />
+            </Tabs.Panel>
+
+            <Tabs.Panel value='register'>
+              <RegisterForm />
+            </Tabs.Panel>
+
+          </Tabs>
+
+        </Stack>
 
       </Drawer>
     </>

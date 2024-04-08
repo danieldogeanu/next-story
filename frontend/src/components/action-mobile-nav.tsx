d120b54f5731aea496192ed0e0a175ef8d075fd0
Daniel Dogeanu <contact@danieldogeanu.com>
2024-04-08 13:12:13 +0300

    add site-drawer to mobile nav action

--- frontend/src/components/action-mobile-nav.tsx
+++ frontend/src/components/action-mobile-nav.tsx
@@ -1,14 +1,31 @@
 'use client';
 
 import { IconMenuDeep } from '@tabler/icons-react';
+import { useDisclosure } from '@mantine/hooks';
 import ActionEntry from '@/components/action-entry';
+import SiteDrawer from '@/components/site-drawer';
+import styles from '@/styles/action-mobile-nav.module.scss';
 
 export default function ActionMobileNav({...props}) {
+  const [opened, {open, close}] = useDisclosure(false);
+
   return (
-    <ActionEntry
-      label='Navigation'
-      icon={IconMenuDeep}
-      {...props}
-    />
+    <>
+      <ActionEntry
+        label='Navigation'
+        icon={IconMenuDeep}
+        variant={opened ? 'light' : 'subtle'}
+        onClick={open}
+        {...props}
+      />
+
+      <SiteDrawer
+        title='Navigation'
+        size={300}
+        opened={opened}
+        onClose={close}>
+
+      </SiteDrawer>
+    </>
   );
 }

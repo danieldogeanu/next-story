a8b49cb5e5466cf86565ae70fe48bceaa1b72bb6
Daniel Dogeanu <contact@danieldogeanu.com>
2024-04-11 20:51:25 +0300

    move social nav entries to mock data

--- frontend/src/components/socialnav.tsx
+++ frontend/src/components/socialnav.tsx
@@ -1,21 +1,16 @@
 'use client';
 
 import SocialIcon, { SocialIconProps } from '@/components/social-icon';
-import { IconBrandDribbble, IconBrandGithub, IconBrandInstagram, IconBrandLinkedin } from '@tabler/icons-react';
+import { mockSocialNavEntries } from '@/data/mock';
 import styles from '@/styles/socialnav.module.scss';
 
 export default function SocialNav() {
-  const socialIcons: SocialIconProps[] = [
-    { icon: IconBrandLinkedin, label: 'LinkedIn', href: 'https://linkedin.com/in/danieldogeanu' },
-    { icon: IconBrandGithub, label: 'GitHub', href: 'https://github.com/danieldogeanu' },
-    { icon: IconBrandDribbble, label: 'Dribbble', href: 'https://dribbble.com/danieldogeanu' },
-    { icon: IconBrandInstagram, label: 'Instagram', href: 'https://instagram.com/danieldogeanu' },
-  ];
+  const socialIcons: SocialIconProps[] = mockSocialNavEntries;
 
   return (
     <nav className={styles.container}>
       {socialIcons && socialIcons.map((socialIcon, index) => (
-        <SocialIcon key={index} icon={socialIcon.icon} label={socialIcon.label} href={socialIcon.href} />
+        <SocialIcon key={index} {...socialIcon} />
       ))}
     </nav>
   );
--- frontend/src/data/mock.tsx
+++ frontend/src/data/mock.tsx
@@ -1,6 +1,11 @@
-import { IconCategory, IconHome, IconInfoSquareRounded, IconMail } from '@tabler/icons-react';
+import {
+  IconBrandDribbble, IconBrandGithub, IconBrandInstagram, IconBrandLinkedin,
+  IconCategory, IconHome, IconInfoSquareRounded, IconMail
+} from '@tabler/icons-react';
 import { NavEntryProps } from '@/data/types';
+import { SocialIconProps } from '@/components/social-icon';
 
+/** Main site navigation entries. */
 export const mockSiteNavEntries: NavEntryProps[] = [
   { href: '/', label: 'Home', icon: <IconHome /> },
   { href: '/about', label: 'About', icon: <IconInfoSquareRounded /> },
@@ -16,4 +21,12 @@ export const mockSiteNavEntries: NavEntryProps[] = [
     { href: '/categories/fifth', label: 'Fifth' },
   ] },
   { href: '/contact', label: 'Contact', icon: <IconMail /> },
+];
+
+/** Footer social navigation entries. */
+export const mockSocialNavEntries: SocialIconProps[] = [
+  { icon: IconBrandLinkedin, label: 'LinkedIn', href: 'https://linkedin.com/in/danieldogeanu' },
+  { icon: IconBrandGithub, label: 'GitHub', href: 'https://github.com/danieldogeanu' },
+  { icon: IconBrandDribbble, label: 'Dribbble', href: 'https://dribbble.com/danieldogeanu' },
+  { icon: IconBrandInstagram, label: 'Instagram', href: 'https://instagram.com/danieldogeanu' },
 ];

7d53ed2837cf2a0e62912e1cd3ef28b1daded30e
Daniel Dogeanu <contact@danieldogeanu.com>
2024-04-20 00:04:54 +0300

    refactor dynamic-icon to be a client component

--- frontend/src/components/dynamic-icon.tsx
+++ frontend/src/components/dynamic-icon.tsx
@@ -1,26 +1,32 @@
-'use server';
+'use client';
 
-import { Icon, TablerIconsProps } from "@tabler/icons-react";
+import { TablerIconsProps } from '@tabler/icons-react';
+import { useState, useEffect, useTransition } from 'react';
 
 export interface DynamicIconProps extends TablerIconsProps {
   icon: string;
-}
+};
+
+export interface TablerIcons {
+  [key: string]: React.FC<TablerIconsProps>;
+};
 
 /**
- * React component that generates TablerIcons dynamically.
+ * Client React component that generates TablerIcons dynamically from a string.
  * 
  * @param props The React props for the dynamically loaded icon.
  * @returns Returns a promise of the JSX.Element that can be used as SVG Icon in any React component.
  */
-export default async function DynamicIcon({icon, ...props}: DynamicIconProps) {
-  const TablerIcons = await import('@tabler/icons-react');
+export default function DynamicIcon({icon, ...props}: DynamicIconProps) {
+  const [isPending, startTransition] = useTransition();
+  const [SelectedIcon, selectIcon] = useState<React.FC<TablerIconsProps> | null>(null);
 
-  // // Get key name to type the icon prop correctly.
-  type TablerIconsType = Awaited<typeof TablerIcons>;
-  type IconName = keyof TablerIconsType;
+  useEffect(() => {
+    startTransition(async () => {
+      const tablerIcons = (await import('@tabler/icons-react')) as unknown as TablerIcons;
+      selectIcon(() => tablerIcons[icon]);
+    });
+  }, [icon]);
 
-  // // Select the correct icon and return it.
-  const SelectedIcon = TablerIcons[icon as IconName] as Icon;
-
-  return <SelectedIcon {...props} />;
-}
+  return SelectedIcon ? <SelectedIcon {...props} /> : null;
+};

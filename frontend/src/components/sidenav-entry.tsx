a7a5feb283958bff91b7c6c66c34f198b863dec6
Daniel Dogeanu <contact@danieldogeanu.com>
2024-04-20 00:07:45 +0300

    fix sidenav-entry component

--- frontend/src/components/sidenav-entry.tsx
+++ frontend/src/components/sidenav-entry.tsx
@@ -1,18 +1,19 @@
 'use client';
 
-import { NavLink } from '@mantine/core';
-import Link from 'next/link';
+import Link, { LinkProps } from 'next/link';
+import { NavLink, NavLinkProps } from '@mantine/core';
 import { usePathname } from 'next/navigation';
-import { NavEntryProps } from '@/data/types';
+import { NavEntryItem } from '@/data/types';
+import DynamicIcon from '@/components/dynamic-icon';
 import styles from '@/styles/sidenav.module.scss';
 
-export default function SideNavEntry({href, label, icon, submenu, ...props}: NavEntryProps) {
+export interface SideNavEntryProps extends NavEntryItem, Omit<NavLinkProps, 'label'>, Omit<LinkProps, 'href'> {}
+
+export default function SideNavEntry({href, label, icon, submenu, ...props}: SideNavEntryProps) {
   const pathname = usePathname();
   const isActive = (href: string) => (pathname === href);
   const hasSubmenu = () => (submenu !== undefined && submenu.length > 0);
-  const propsWithIcon: Partial<NavEntryProps> = icon ? {...props, leftSection: icon} : props;
-
-  // TODO: Here we need to pass the icon as such: `{...props, leftSection: <TablerIcon />}`.
+  const propsWithIcon: Partial<SideNavEntryProps> = icon ? {...props, leftSection: <DynamicIcon icon={icon} />} : props;
 
   return hasSubmenu() ? (
     <NavLink

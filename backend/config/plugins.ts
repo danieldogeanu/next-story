a20727e6b549ea62a9f8cbfef2ad268f4c6173af
Daniel Dogeanu <contact@danieldogeanu.com>
2024-04-22 09:17:42 +0300

    install strapi plugins

--- backend/config/plugins.ts
+++ backend/config/plugins.ts
@@ -1,1 +1,14 @@
-export default () => ({});
+export default () => ({
+  graphql: {
+    config: {
+      endpoint: '/graphql',
+      shadowCRUD: true,
+      playgroundAlways: false,
+      depthLimit: 10,
+      amountLimit: 100,
+      apolloServer: {
+        tracing: false,
+      },
+    },
+  },
+});

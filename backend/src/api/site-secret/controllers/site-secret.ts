28143624c9b40544f27f939d95a9212e28645818
Daniel Dogeanu <contact@danieldogeanu.com>
2024-04-23 12:51:04 +0300

    add site secrets collection schema

---
+++ backend/src/api/site-secret/controllers/site-secret.ts
@@ -0,0 +1,7 @@
+/**
+ * site-secret controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::site-secret.site-secret');
---
+++ backend/src/api/site-secret/routes/site-secret.ts
@@ -0,0 +1,7 @@
+/**
+ * site-secret router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::site-secret.site-secret');

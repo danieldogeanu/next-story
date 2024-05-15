7c1c588db76ba26f85d13f143ea3369203b17e53
Daniel Dogeanu <contact@danieldogeanu.com>
2024-05-16 00:27:03 +0300

    add function to fetch strapi collection

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,9 +1,14 @@
+import { APIResponseCollection } from '@/types/strapi';
+import { fetchMany } from '@/data/strapi';
 import styles from '@/styles/page.module.scss';
 
-export default function Home() {
+export default async function Home() {
+  const data = (await fetchMany('api::article.article')) as APIResponseCollection<'api::article.article'>;
+  console.log(data);
+
   return (
     <main className={styles.main}>
-      
+
     </main>
   );
 }

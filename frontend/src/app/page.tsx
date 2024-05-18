3528e0774257d2e6b697898a9b6f9bd0c400e47b
Daniel Dogeanu <contact@danieldogeanu.com>
2024-05-19 00:32:49 +0300

    remove temporary request from homepage

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,13 +1,6 @@
-import { APIResponseCollection } from '@/types/strapi';
-import { strapiSDK } from '@/data/strapi-sdk';
 import styles from '@/styles/page.module.scss';
 
 export default async function Home() {
-  // Temporary request to Strapi to test things out.
-  const strapiInstance = await strapiSDK();
-  const articles = await strapiInstance.find<APIResponseCollection<'api::article.article'>>('articles');
-  console.log(articles);
-
   return (
     <main className={styles.main}>
 

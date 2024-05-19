dab67debe5069cbf50b0fb0241d6c312dda34a46
Daniel Dogeanu <contact@danieldogeanu.com>
2024-05-19 05:09:19 +0300

    replace sitenav mock data with strapi data

--- frontend/src/components/sitenav.tsx
+++ frontend/src/components/sitenav.tsx
@@ -1,10 +1,42 @@
-import { mockSiteNavEntries } from '@/data/mock';
 import NavEntry, { NavEntryProps } from '@/components/nav-entry';
+import { StrapiRequestParams } from 'strapi-sdk-js';
+import { strapiSDK } from '@/data/strapi';
+import { GetValues } from '@/types/strapi';
 import styles from '@/styles/sitenav.module.scss';
 
-export default function SiteNav() {
-  // TODO: Replace mockup data with entries from the server.
-  const navEntries: NavEntryProps[] = mockSiteNavEntries;
+export interface StrapiNavRequestParams extends StrapiRequestParams {
+  type?: 'FLAT' | 'TREE' | 'RFR';
+  orderBy?: string;
+  orderDirection?: string;
+}
+
+export type StrapiNavResponse = GetValues<'plugin::navigation.navigation-item'> & {
+  items: StrapiNavResponse[];
+};
+
+export default async function SiteNav() {
+
+  // Make request to server to get main navigation.
+  const strapiInstance = await strapiSDK();
+  const navRequestParams: StrapiNavRequestParams = {type: 'TREE', orderBy: 'order'};
+  const strapiResponse = await strapiInstance.find(
+    'navigation/render/main-navigation', navRequestParams
+  ) as unknown as StrapiNavResponse[];
+
+  // Map the Strapi response to match the NavEntry props shape.
+  const navEntries = strapiResponse.map((item) => {
+    const entry: NavEntryProps = {
+      href: item.path || '',
+      label: item.title,
+    };
+
+    if (item.items) entry.submenu = item.items.map((subItem) => ({
+      href: subItem.path || '',
+      label: subItem.title,
+    }));
+
+    return entry;
+  }) as NavEntryProps[];
 
   return (
     <nav className={styles.container}>

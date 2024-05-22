95d733d1cab79ff9998066e32ee5d526e431c279
Daniel Dogeanu <contact@danieldogeanu.com>
2024-05-22 04:17:12 +0300

    add function to get authors from strapi

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,21 +1,7 @@
-import { getArticlesCollection, getSingleArticle } from '@/data/articles';
+import { getAuthorsCollection, getSingleAuthor } from '@/data/authors';
 import styles from '@/styles/page.module.scss';
 
 export default async function Home() {
-
-  // const strapiResponse = await getArticlesCollection({pagination: {page: 1, pageSize: 2}});
-
-  // console.log(strapiResponse);
-
-  // strapiResponse.data.map((article) => {
-  //   console.log(article);
-  // });
-
-
-  const strapiResponse = await getSingleArticle(5, {fields: ['title', 'slug', 'excerpt']});
-
-  console.log(strapiResponse);
-
   return (
     <main className={styles.main}>
 
---
+++ frontend/src/data/authors.ts
@@ -0,0 +1,50 @@
+import { StrapiRequestParams } from 'strapi-sdk-js';
+import { APIResponse, APIResponseCollection } from '@/types/strapi';
+import { strapiSDK } from '@/data/strapi';
+
+export interface SingleAuthorResponse extends APIResponse<'api::author.author'> {}
+
+export interface AuthorsCollectionResponse extends APIResponseCollection<'api::author.author'> {}
+
+/**
+ * Fetches a single author from the Strapi backend by their ID.
+ *
+ * @param {string | number} id - The ID of the author to fetch.
+ * @param {StrapiRequestParams} [params] - Optional parameters for the request.
+ * @returns A promise that resolves to the author data.
+ * @throws {Error} Throws an error if the fetch request fails.
+ *
+ * @example
+ * // Fetch a single author by ID.
+ * await getSingleAuthor(1);
+ *
+ * @example
+ * // Fetch a single author by ID with custom parameters.
+ * await getSingleAuthor(1, {fields: ['slug', 'biography', 'fullName']});
+ */
+export async function getSingleAuthor(id: string | number, params?: StrapiRequestParams): Promise<SingleAuthorResponse> {
+  const strapiInstance = await strapiSDK();
+  const strapiResponse = await strapiInstance.findOne('authors', id, params) as SingleAuthorResponse;
+  return strapiResponse;
+}
+
+/**
+ * Fetches a collection of authors from the Strapi backend.
+ *
+ * @param {StrapiRequestParams} [params] - Optional parameters for the request.
+ * @returns A promise that resolves to a collection of authors.
+ * @throws {Error} Throws an error if the fetch request fails.
+ *
+ * @example
+ * // Fetch a collection of authors.
+ * await getAuthorsCollection();
+ *
+ * @example
+ * // Fetch a collection of authors with custom parameters.
+ * await getAuthorsCollection({pagination: {page: 1, pageSize: 2}});
+ */
+export async function getAuthorsCollection(params?: StrapiRequestParams): Promise<AuthorsCollectionResponse> {
+  const strapiInstance = await strapiSDK();
+  const strapiResponse = await strapiInstance.find('authors', params) as unknown as AuthorsCollectionResponse;
+  return strapiResponse;
+}

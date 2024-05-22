24bf7f42f7378db206556492e34c1f9a3d32e42c
Daniel Dogeanu <contact@danieldogeanu.com>
2024-05-22 03:22:21 +0300

    add function to get articles from strapi

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,6 +1,21 @@
+import { getArticlesCollection, getSingleArticle } from '@/data/articles';
 import styles from '@/styles/page.module.scss';
 
 export default async function Home() {
+
+  // const strapiResponse = await getArticlesCollection({pagination: {page: 1, pageSize: 2}});
+
+  // console.log(strapiResponse);
+
+  // strapiResponse.data.map((article) => {
+  //   console.log(article);
+  // });
+
+
+  const strapiResponse = await getSingleArticle(5, {fields: ['title', 'slug', 'excerpt']});
+
+  console.log(strapiResponse);
+
   return (
     <main className={styles.main}>
 
---
+++ frontend/src/data/articles.ts
@@ -0,0 +1,50 @@
+import { StrapiRequestParams } from 'strapi-sdk-js';
+import { APIResponse, APIResponseCollection } from '@/types/strapi';
+import { strapiSDK } from '@/data/strapi';
+
+export interface SingleArticleResponse extends APIResponse<'api::article.article'> {}
+
+export interface ArticlesCollectionResponse extends APIResponseCollection<'api::article.article'> {}
+
+/**
+ * Fetches a single article from the Strapi backend by its ID.
+ *
+ * @param {string | number} id - The ID of the article to fetch.
+ * @param {StrapiRequestParams} [params] - Optional parameters for the request.
+ * @returns A promise that resolves to the article data.
+ * @throws {Error} Throws an error if the fetch request fails.
+ *
+ * @example
+ * // Fetch a single article by ID.
+ * await getSingleArticle(3);
+ *
+ * @example
+ * // Fetch a single article by ID with custom parameters.
+ * await getSingleArticle(3, {fields: ['title', 'slug', 'excerpt']});
+ */
+export async function getSingleArticle(id: string | number, params?: StrapiRequestParams): Promise<SingleArticleResponse> {
+  const strapiInstance = await strapiSDK();
+  const strapiResponse = await strapiInstance.findOne('articles', id, params) as SingleArticleResponse;
+  return strapiResponse;
+}
+
+/**
+ * Fetches a collection of articles from the Strapi backend.
+ *
+ * @param {StrapiRequestParams} [params] - Optional parameters for the request.
+ * @returns A promise that resolves to a collection of articles.
+ * @throws {Error} Throws an error if the fetch request fails.
+ *
+ * @example
+ * // Fetch a collection of articles.
+ * await getArticlesCollection();
+ *
+ * @example
+ * // Fetch a collection of articles with custom parameters.
+ * await getArticlesCollection({pagination: {page: 1, pageSize: 2}});
+ */
+export async function getArticlesCollection(params?: StrapiRequestParams): Promise<ArticlesCollectionResponse> {
+  const strapiInstance = await strapiSDK();
+  const strapiResponse = await strapiInstance.find('articles', params) as unknown as ArticlesCollectionResponse;
+  return strapiResponse;
+}

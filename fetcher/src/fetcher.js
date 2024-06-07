36a57231ff16881271212d4be38a87cf0d0474a3
Daniel Dogeanu <contact@danieldogeanu.com>
2024-06-07 16:30:48 +0300

    add function to get data from strapi

--- fetcher/src/fetcher.js
+++ fetcher/src/fetcher.js
@@ -1,22 +1,23 @@
 import fs from 'node:fs';
 import path from 'node:path';
 import * as dotenv from 'dotenv';
-import { getNodeEnv } from './utils.js';
+import { getNodeEnv, getAPIKey } from './utils.js';
+import { strapiSDK } from './strapi.js';
 
 /**
  * List of endpoint objects to fetch data from. The endpoints should shave the following shape:
- * {{endpoint: string, for: 'frontend' | 'backend', query: Object}[]}
+ * {{endpoint: string, keyFor: 'frontend' | 'backend', params: Object}[]}
  */
 const ENDPOINTS = [
-  {endpoint: 'articles', for: 'frontend', query: {populate: '*'}},
-  {endpoint: 'authors', for: 'frontend', query: {populate: '*'}},
-  {endpoint: 'categories', for: 'frontend', query: {populate: '*'}},
-  {endpoint: 'categories', for: 'frontend', query: {populate: '*'}},
-  {endpoint: 'pages', for: 'frontend', query: {populate: '*'}},
-  {endpoint: 'tags', for: 'frontend', query: {populate: '*'}},
-  {endpoint: 'upload/files', for: 'frontend', query: {populate: '*'}},
-  {endpoint: 'navigation', for: 'frontend', query: {populate: '*'}},
-  {endpoint: 'site-setting', for: 'frontend', query: {populate: '*'}},
+  {endpoint: 'articles', keyFor: 'frontend', params: {populate: '*'}},
+  {endpoint: 'authors', keyFor: 'frontend', params: {populate: '*'}},
+  {endpoint: 'categories', keyFor: 'frontend', params: {populate: '*'}},
+  {endpoint: 'categories', keyFor: 'frontend', params: {populate: '*'}},
+  {endpoint: 'pages', keyFor: 'frontend', params: {populate: '*'}},
+  {endpoint: 'tags', keyFor: 'frontend', params: {populate: '*'}},
+  {endpoint: 'upload/files', keyFor: 'frontend', params: {populate: '*'}},
+  {endpoint: 'navigation', keyFor: 'frontend', params: {populate: '*'}},
+  {endpoint: 'site-setting', keyFor: 'frontend', params: {populate: '*'}},
 ];
 const DATA_DIR = '../data';
 
@@ -27,4 +28,34 @@ if (getNodeEnv() === 'localhost') {
   });
 }
 
+/**
+ * Fetches data from a specified endpoint in the Strapi backend.
+ *
+ * @param {Object} requestObj - The requestObj object.
+ * @param {string} requestObj.endpoint - The Strapi API endpoint to fetch data from.
+ * @param {'frontend' | 'backend'} requestObj.keyFor - The type of API key to use ('frontend' or 'backend').
+ * @param {Object} [requestObj.params] - Optional parameters for the request.
+ * @returns {Promise<Object>} A promise that resolves to the fetched data.
+ * @throws {Error} Logs an error if the data fetching fails.
+ *
+ * @example
+ * // Fetch data from a specific endpoint.
+ * await getData({
+ *   endpoint: 'articles',
+ *   keyFor: 'frontend',
+ *   params: { populate: '*', sort: 'createdAt:desc' }
+ * });
+ */
+async function getData({endpoint, keyFor, params}) {
+  try {
+    const strapiInstance = await strapiSDK(await getAPIKey(keyFor));
+    const strapiResponse = await strapiInstance.find(endpoint, params);
+    return strapiResponse;
+  } catch (error) {
+    console.error(`Error fetching data from /${endpoint}:`, error.message);
+  }
+}
 
+console.log(await getData(
+  {endpoint: 'upload/files', keyFor: 'frontend', params: {populate: '*', limit: 4}},
+));

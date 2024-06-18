5efc5c13e453c620be091a2bc19192a6c33f3abe
Daniel Dogeanu <contact@danieldogeanu.com>
2024-06-18 08:40:51 +0300

    add data request for page route

--- frontend/src/app/[slug]/page.tsx
+++ frontend/src/app/[slug]/page.tsx
@@ -1,4 +1,5 @@
 import { Title } from '@mantine/core';
+import { getPagesCollection } from '@/data/pages';
 import styles from '@/styles/page.module.scss';
 
 export interface PageProps {
@@ -8,11 +9,16 @@ export interface PageProps {
 }
 
 export default async function Page({params}: PageProps) {
+  const pageData = (await getPagesCollection({
+    populate: '*', filters: { slug: { $eq: params.slug } }
+  })).data.pop()?.attributes;
+
   return (
     <main className={styles.main}>
       <Title className={styles.pageTitle}>
-        {params.slug.replace('-', ' ')}
+        {pageData?.title}
       </Title>
+      <p>{pageData?.excerpt}</p>
     </main>
   );
 }

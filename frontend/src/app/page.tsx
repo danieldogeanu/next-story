64b46f1d12fd9eca470e00c665345bcb362eed3a
Daniel Dogeanu <contact@danieldogeanu.com>
2024-06-18 07:33:13 +0300

    add page title for page

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,9 +1,10 @@
+import { Title } from '@mantine/core';
 import styles from '@/styles/page.module.scss';
 
 export default async function Home() {
   return (
     <main className={styles.main}>
-
+      <Title className={styles.pageTitle}>Home</Title>
     </main>
   );
 }

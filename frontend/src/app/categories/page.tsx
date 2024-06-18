16113d2427eb8347f0fbe8ba53138c0de0a77a13
Daniel Dogeanu <contact@danieldogeanu.com>
2024-06-18 11:42:55 +0300

    change response variable names

--- frontend/src/app/categories/page.tsx
+++ frontend/src/app/categories/page.tsx
@@ -3,12 +3,12 @@ import { getCategoriesCollection } from '@/data/categories';
 import styles from '@/styles/page.module.scss';
 
 export default async function CategoriesPage() {
-  const categoriesResponse = await getCategoriesCollection({populate: '*'});
+  const categoriesCollection = await getCategoriesCollection({populate: '*'});
 
   return (
     <main className={styles.main}>
       <Title className={styles.pageTitle}>Categories</Title>
-      <ul>{categoriesResponse.data.map((category) => {
+      <ul>{categoriesCollection.data.map((category) => {
         const categoryData = category.attributes;
         return (<li key={category.id}>
           <h4>{categoryData.name}</h4>
--- frontend/src/app/tags/page.tsx
+++ frontend/src/app/tags/page.tsx
@@ -4,12 +4,12 @@ import { getTagsCollection } from '@/data/tags';
 import styles from '@/styles/page.module.scss';
 
 export default async function TagsPage() {
-  const tagsResponse = await getTagsCollection({populate: '*'});
+  const tagsCollection = await getTagsCollection({populate: '*'});
 
   return (
     <main className={styles.main}>
       <Title className={styles.pageTitle}>Tags</Title>
-      <ul>{tagsResponse.data.map((tag) => {
+      <ul>{tagsCollection.data.map((tag) => {
         const tagData = tag.attributes;
         return (<li key={tag.id}>
           <Link href={`/tags/${tagData.slug}`}>{tagData.name}</Link>

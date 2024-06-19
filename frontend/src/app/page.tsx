0cfc4c3aebbe74cc46d97633ffc7836b2f3771bb
Daniel Dogeanu <contact@danieldogeanu.com>
2024-06-19 13:03:50 +0300

    add article card to homepage

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,8 +1,5 @@
-import Link from 'next/link';
-import path from 'node:path';
-import { Title } from '@mantine/core';
 import { getArticlesCollection } from '@/data/articles';
-import { convertToUnixTime } from '@/utils/date';
+import ArticleCard from '@/components/article-card';
 import styles from '@/styles/page.module.scss';
 
 export default async function Home() {
@@ -10,14 +7,11 @@ export default async function Home() {
 
   return (
     <main className={styles.main}>
-      <Title className={styles.pageTitle}>Home</Title>
-      {articlesCollection.data.map((article) => {
-        const articleData = article.attributes;
-        const articleHref = path.join('articles', convertToUnixTime(articleData.createdAt), articleData.slug);
-        return (<li key={article.id}>
-          <Link href={articleHref}>{articleData.title}</Link>
-        </li>);
-      })}
+      <section className={styles.grid}>
+        {articlesCollection.data.map((article) => {
+          return (<ArticleCard key={article.id} data={article.attributes} />);
+        })}
+      </section>
     </main>
   );
 }
---
+++ frontend/src/components/article-card.tsx
@@ -0,0 +1,59 @@
+import NextImage from 'next/image';
+import Link from 'next/link';
+import path from 'node:path';
+import { ActionIcon, Card, CardSection, Group, Image, Text, Title } from '@mantine/core';
+import { IconArrowNarrowRight } from '@tabler/icons-react';
+import { convertToReadableDate, convertToUnixTime } from '@/utils/date';
+import { SingleArticle } from '@/data/articles';
+import { getFileURL } from '@/data/files';
+import styles from '@/styles/article-card.module.scss';
+
+export interface ArticleCardProps {
+  data: SingleArticle;
+}
+
+export default function ArticleCard({data}: ArticleCardProps) {
+  const articleHref = path.join('articles', convertToUnixTime(data.createdAt), data.slug);
+  const articleCover = data?.cover?.data.attributes;
+  const articleCoverFormats = JSON.parse(JSON.stringify(articleCover?.formats));
+  const articleCoverUrl = getFileURL(articleCoverFormats.small.url);
+  const articleCategory = data.category?.data.attributes;
+  const articleAuthor = data.author?.data.attributes;
+  
+  return (
+    <Card
+      className={styles.card}
+      component={Link}
+      href={articleHref}
+      padding='xs'
+      radius='md'>
+
+      <CardSection>
+        <Image
+          component={NextImage}
+          src={articleCoverUrl}
+          width={articleCoverFormats.small.width}
+          height={articleCoverFormats.small.height}
+          alt={articleCover?.alternativeText || 'No Description'}
+          h={200} radius='md' />
+      </CardSection>
+      
+      <Group className={styles.meta} justify='space-between'>
+        <Text>{articleCategory?.name}</Text>
+        <Text>{convertToReadableDate(data.createdAt)}</Text>
+      </Group>
+
+      <Title className={styles.title} order={2}>
+        {data.title.substring(0, 60)}
+      </Title>
+
+      <Group className={styles.author} justify='space-between'>
+        <Text>{articleAuthor?.fullName}</Text>
+        <ActionIcon size='lg' variant='subtle' aria-label='Read Article'>
+          <IconArrowNarrowRight size={24} stroke={1.5} />
+        </ActionIcon>
+      </Group>
+
+    </Card>
+  );
+}

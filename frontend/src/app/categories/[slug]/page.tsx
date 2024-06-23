6d0ea068e4f9d7c404d4479c2315a767282e6215
Daniel Dogeanu <contact@danieldogeanu.com>
2024-06-23 17:47:23 +0300

    add article cards to category page

--- frontend/src/app/categories/[slug]/page.tsx
+++ frontend/src/app/categories/[slug]/page.tsx
@@ -1,7 +1,9 @@
+import classNames from 'classnames';
 import { notFound } from 'next/navigation';
 import { Title } from '@mantine/core';
 import { getCategoriesCollection } from '@/data/categories';
 import { capitalize } from '@/utils/strings';
+import ArticleCard from '@/components/article-card';
 import pageStyles from '@/styles/page.module.scss';
 import categoryStyles from '@/styles/category-page.module.scss';
 
@@ -13,19 +15,33 @@ export interface CategoryPageProps {
 
 export default async function CategoryPage({params}: CategoryPageProps) {
   const categoryData = (await getCategoriesCollection({
-    populate: '*', filters: { slug: { $eq: params.slug } }
+    filters: { slug: { $eq: params.slug } },
+    populate: {
+      cover: { populate: '*' },
+      seo: { populate: '*' },
+      articles: { populate: '*' },
+    },
   })).data.pop()?.attributes;
+  const articlesData = categoryData?.articles?.data;
 
   if (!categoryData) return notFound();
 
   return (
     <main className={pageStyles.main}>
-      <section className={pageStyles.container}>
+
+      <section className={classNames(pageStyles.container, categoryStyles.intro)}>
         <Title className={pageStyles.pageTitle}>
           {capitalize(categoryData?.name)} Category
         </Title>
         <p>{categoryData?.description}</p>
       </section>
+
+      <section className={pageStyles.grid}>
+        {articlesData?.map((article) => {
+          return <ArticleCard key={article.id} data={article.attributes} />;
+        })}
+      </section>
+      
     </main>
   );
 }

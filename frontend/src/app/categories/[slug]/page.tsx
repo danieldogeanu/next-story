81606f4320adcde9fecdbdf2936b4a6158851644
Daniel Dogeanu <contact@danieldogeanu.com>
2024-06-23 20:35:58 +0300

    add hero section to category page

--- frontend/src/app/categories/[slug]/page.tsx
+++ frontend/src/app/categories/[slug]/page.tsx
@@ -1,8 +1,10 @@
+import NextImage from 'next/image';
 import classNames from 'classnames';
 import { notFound } from 'next/navigation';
-import { Title } from '@mantine/core';
+import { Title, Image, Box, Text } from '@mantine/core';
 import { getCategoriesCollection } from '@/data/categories';
 import { capitalize } from '@/utils/strings';
+import { getFileURL } from '@/data/files';
 import ArticleCard from '@/components/article-card';
 import pageStyles from '@/styles/page.module.scss';
 import categoryStyles from '@/styles/category-page.module.scss';
@@ -22,7 +24,12 @@ export default async function CategoryPage({params}: CategoryPageProps) {
       articles: { populate: '*' },
     },
   })).data.pop()?.attributes;
+  const categoryCover = categoryData?.cover?.data.attributes;
+  const categoryCoverFormats = JSON.parse(JSON.stringify(categoryCover?.formats));
+  const categoryCoverUrl = getFileURL(categoryCover?.url as string);
   const articlesData = categoryData?.articles?.data;
+
+  console.log(categoryCover);
 
   if (!categoryData) return notFound();
 
@@ -30,10 +37,25 @@ export default async function CategoryPage({params}: CategoryPageProps) {
     <main className={pageStyles.main}>
 
       <section className={classNames(pageStyles.container, categoryStyles.intro)}>
+
         <Title className={pageStyles.pageTitle}>
           {capitalize(categoryData?.name)} Category
         </Title>
-        <p>{categoryData?.description}</p>
+
+        <Box className={categoryStyles.hero}>
+          <Box className={categoryStyles.description}>
+            <Text>{categoryData?.description}</Text>
+          </Box>
+          <Image
+            className={categoryStyles.cover}
+            component={NextImage}
+            src={categoryCoverUrl}
+            width={categoryCover?.width}
+            height={categoryCover?.height}
+            alt={categoryCover?.alternativeText || ''}
+            h={300} radius='lg' />
+        </Box>
+
       </section>
 
       <section className={pageStyles.grid}>
@@ -41,7 +63,7 @@ export default async function CategoryPage({params}: CategoryPageProps) {
           return <ArticleCard key={article.id} data={article.attributes} />;
         })}
       </section>
-      
+
     </main>
   );
 }

187ab7e3e13f49f18792b066e740e841e91472d8
Daniel Dogeanu <contact@danieldogeanu.com>
2024-07-05 19:47:02 +0300

    make sort bar buttons larger on mobile

--- frontend/src/components/sort-bar.tsx
+++ frontend/src/components/sort-bar.tsx
@@ -2,11 +2,16 @@
 
 import { ActionIcon, Box, Group, Select, Text } from '@mantine/core';
 import { IconChevronDown, IconChevronUp } from '@tabler/icons-react';
+import { useMediaQuery } from '@mantine/hooks';
 import classNames from 'classnames';
 import styles from '@/styles/sort-bar.module.scss';
 
 
 export default function SortBar() {
+  const isMobile = useMediaQuery(`(max-width: 480px)`, false);
+  const actionButtonSize = isMobile ? 'lg' : 'md';
+  const actionIconSize = isMobile ? 36 : 24;
+  const sortSelectSize = isMobile ? 'md' : 'sm';
   const sortSelectData = [
     { value: 'publishedAt', label: 'Published Date' },
     { value: 'createdAt', label: 'Created Date' },
@@ -22,23 +27,26 @@ export default function SortBar() {
       <Group className={styles.order} justify='flex-end' gap={0}>
         <ActionIcon
           className={classNames(styles.chevron, styles.up)}
+          size={actionButtonSize}
           variant='transparent'
           title='Ascending'
           aria-label='Ascending'>
-          <IconChevronUp size={24} stroke={1.5} />
+          <IconChevronUp size={actionIconSize} stroke={1.5} />
         </ActionIcon>
         <ActionIcon
           className={classNames(styles.chevron, styles.down)}
+          size={actionButtonSize}
           variant='transparent'
           title='Descending'
           aria-label='Descending'>
-          <IconChevronDown size={24} stroke={1.5} />
+          <IconChevronDown size={actionIconSize} stroke={1.5} />
         </ActionIcon>
       </Group>
 
       <Select
         className={styles.sort}
         data={sortSelectData}
+        size={sortSelectSize}
         defaultValue='publishedAt'
         withCheckIcon={false}
         aria-label='Sort by:'

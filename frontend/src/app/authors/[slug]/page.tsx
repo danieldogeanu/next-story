605b2584e8034e00d70cf1350c13ce5fc4e49793
Daniel Dogeanu <contact@danieldogeanu.com>
2024-07-10 23:48:52 +0300

    add types & pull necessary data to author page

--- frontend/src/app/authors/[slug]/page.tsx
+++ frontend/src/app/authors/[slug]/page.tsx
@@ -1,7 +1,8 @@
 import classNames from 'classnames';
 import { notFound } from 'next/navigation';
 import { Title } from '@mantine/core';
-import { getAuthorsCollection } from '@/data/authors';
+import { getAuthorsCollection, SingleAuthor } from '@/data/authors';
+import { getFileURL } from '@/data/files';
 import { capitalize } from '@/utils/strings';
 import pageStyles from '@/styles/page.module.scss';
 import authorStyles from '@/styles/author-page.module.scss';
@@ -12,10 +13,28 @@ export interface AuthorPageProps {
   };
 }
 
+// TODO: Fix these types, because they break if a property is undefined.
+type AuthorAvatar = NonNullable<SingleAuthor['avatar']>['data']['attributes'];
+type AuthorSocials = NonNullable<SingleAuthor['socialNetworks']>;
+type AuthorSocialEntry = AuthorSocials[number] & {id: number};
+type AuthorArticlesData = NonNullable<SingleAuthor['articles']>['data'];
+
 export default async function AuthorPage({params}: AuthorPageProps) {
   const authorData = (await getAuthorsCollection({
-    populate: '*', filters: { slug: { $eq: params.slug } }
+    filters: { slug: { $eq: params.slug } },
+    populate: {
+      avatar: { populate: '*' },
+      socialNetworks: { populate: '*' },
+      articles: { populate: '*' },
+      seo: { populate: '*' },
+    },
   })).data.pop()?.attributes;
+  const authorAvatar = authorData?.avatar?.data.attributes as AuthorAvatar;
+  const authorAvatarFormats = JSON.parse(JSON.stringify(authorAvatar?.formats));
+  const authorAvatarUrl = getFileURL(authorAvatarFormats.small.url);
+  const authorSocials = authorData?.socialNetworks as unknown as AuthorSocialEntry[];
+  const authorArticlesData = authorData?.articles?.data as AuthorArticlesData;
+  const authorArticlesNumber = (authorArticlesData && authorArticlesData.length) ? authorArticlesData.length : 0;
 
   if (!authorData) return notFound();
 
@@ -27,7 +46,7 @@ export default async function AuthorPage({params}: AuthorPageProps) {
         <Title className={pageStyles.pageTitle}>
           {capitalize(authorData?.fullName) + '\'s'} Articles
         </Title>
-        
+
       </section>
 
     </main>

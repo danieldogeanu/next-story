99d8dbc959715f71e163476b72aa3e9c27123116
Daniel Dogeanu <contact@danieldogeanu.com>
2024-07-15 19:51:41 +0300

    add cover image to article

--- frontend/src/app/articles/[created]/[slug]/page.tsx
+++ frontend/src/app/articles/[created]/[slug]/page.tsx
@@ -1,8 +1,12 @@
+import NextImage from 'next/image';
 import { notFound } from 'next/navigation';
-import { Title } from '@mantine/core';
-import { getArticlesCollection } from '@/data/articles';
+import { Box, Image, Title } from '@mantine/core';
+import { ArticleCover, getArticlesCollection } from '@/data/articles';
+import { StrapiImageFormats } from '@/types/strapi';
 import { convertToISODate } from '@/utils/date';
-import styles from '@/styles/page.module.scss';
+import { getFileURL } from '@/data/files';
+import pageStyles from '@/styles/page.module.scss';
+import articleStyles from '@/styles/article-page.module.scss';
 
 export interface ArticlePageProps {
   params: {
@@ -12,23 +16,49 @@ export interface ArticlePageProps {
 }
 
 export default async function ArticlePage({params}: ArticlePageProps) {
+  // Filters must match both createdAt and slug fields.
   const articleData = (await getArticlesCollection({
     populate: '*', filters: {
       createdAt: { $eq: convertToISODate(params.created) },
       slug: { $eq: params.slug },
     }
   })).data.pop()?.attributes;
+  const articleCover = articleData?.cover?.data?.attributes as ArticleCover;
+  const articleCoverFormats = articleCover?.formats as unknown as StrapiImageFormats;
+  const articleCoverUrl = (articleCoverFormats?.large?.url) ? getFileURL(articleCoverFormats.large.url) : '';
 
-  // Filters must match both createdAt and slug fields.
-  // If not return a 404 Not Found error page.
   if (!articleData) return notFound();
 
   return (
-    <main className={styles.main}>
-      <Title className={styles.pageTitle}>
-        {articleData?.title}
-      </Title>
-      <p>{articleData?.excerpt}</p>
+    <main className={pageStyles.main}>
+
+      <article className={pageStyles.container}>
+
+        <header className={pageStyles.intro}>
+
+          <Title className={pageStyles.pageTitle}>
+            {articleData?.title}
+          </Title>
+
+          <Box className={pageStyles.cover}>
+            <Image
+              className={pageStyles.image}
+              component={NextImage}
+              src={articleCoverUrl}
+              width={articleCoverFormats?.large?.width}
+              height={articleCoverFormats?.large?.height}
+              alt={articleCover?.alternativeText || ''}
+              priority={true} />
+          </Box>
+
+        </header>
+
+        <section className={pageStyles.content}>
+          <p>{articleData?.excerpt}</p>
+        </section>
+
+      </article>
+      
     </main>
   );
 }

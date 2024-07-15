97831bfbffb76450f47a0f6a43e43b2f44522eca
Daniel Dogeanu <contact@danieldogeanu.com>
2024-07-15 17:46:20 +0300

    add cover image to page

--- frontend/src/app/[slug]/page.tsx
+++ frontend/src/app/[slug]/page.tsx
@@ -1,6 +1,9 @@
+import NextImage from 'next/image';
 import { notFound } from 'next/navigation';
-import { Title } from '@mantine/core';
-import { getPagesCollection } from '@/data/pages';
+import { Box, Image, Title } from '@mantine/core';
+import { getPagesCollection, PageCover } from '@/data/pages';
+import { StrapiImageFormats } from '@/types/strapi';
+import { getFileURL } from '@/data/files';
 import styles from '@/styles/page.module.scss';
 
 export interface PageProps {
@@ -13,15 +16,42 @@ export default async function Page({params}: PageProps) {
   const pageData = (await getPagesCollection({
     populate: '*', filters: { slug: { $eq: params.slug } }
   })).data.pop()?.attributes;
+  const pageCover = pageData?.cover?.data?.attributes as PageCover;
+  const pageCoverFormats = pageCover?.formats as unknown as StrapiImageFormats;
+  const pageCoverUrl = (pageCoverFormats?.large?.url) ? getFileURL(pageCoverFormats.large.url) : '';
 
   if (!pageData) return notFound();
 
   return (
     <main className={styles.main}>
-      <Title className={styles.pageTitle}>
-        {pageData?.title}
-      </Title>
-      <p>{pageData?.excerpt}</p>
+
+      <article className={styles.container}>
+
+        <header className={styles.intro}>
+
+          <Title className={styles.pageTitle}>
+            {pageData?.title}
+          </Title>
+
+          <Box className={styles.cover}>
+            <Image
+              className={styles.image}
+              component={NextImage}
+              src={pageCoverUrl}
+              width={pageCoverFormats?.large?.width}
+              height={pageCoverFormats?.large?.height}
+              alt={pageCover?.alternativeText || ''}
+              priority={true} />
+          </Box>
+
+        </header>
+
+        <section className={styles.content}>
+          <p>{pageData?.excerpt}</p>
+        </section>
+
+      </article>
+      
     </main>
   );
 }

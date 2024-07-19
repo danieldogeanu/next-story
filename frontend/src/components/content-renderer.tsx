b49e6c6a42b501dbea51b28b1c5ffa25a89d0bf3
Daniel Dogeanu <contact@danieldogeanu.com>
2024-07-19 03:55:01 +0300

    replace image block for content renderer

--- frontend/src/components/content-renderer.tsx
+++ frontend/src/components/content-renderer.tsx
@@ -1,13 +1,14 @@
 'use client';
 
-import { TypographyStylesProvider } from '@mantine/core';
+import NextImage from 'next/image';
+import { Image, TypographyStylesProvider } from '@mantine/core';
 import { BlocksRenderer, type BlocksContent } from '@strapi/blocks-react-renderer';
+import { StrapiImageFormats } from '@/types/strapi';
+import { getFileURL } from '@/data/files';
 
 export interface ContentRendererProps {
   readonly content: BlocksContent;
 }
-
-// TODO: Replace `image` and `link` components with Next.js/Mantine components.
 
 export default function ContentRenderer({content}: ContentRendererProps) {
   if (!content) return null;
@@ -16,6 +17,27 @@ export default function ContentRenderer({content}: ContentRendererProps) {
     <TypographyStylesProvider>
       <BlocksRenderer
         content={content}
+        blocks={{
+          image: ({image}) => {
+            const imageFormats = image.formats as StrapiImageFormats;
+            const imageUrl = (imageFormats?.large?.url) ? getFileURL(imageFormats.large.url) : '';
+
+            // TODO: Handle the case where image has a caption.
+            // TODO: Open the full resolution in a full screen modal, on click.
+            // TODO: Handle the `srcset` prop, so that we can have the right image for the right resolution.
+
+            return (
+              <Image
+                className='full-width'
+                component={NextImage}
+                src={imageUrl}
+                width={imageFormats?.large?.width}
+                height={imageFormats?.large?.height}
+                alt={image.alternativeText || ''}
+                radius='md' />
+            );
+          },
+        }}
       />
     </TypographyStylesProvider>
   );

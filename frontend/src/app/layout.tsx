656c90d31b857c5206fc0f603364f03687e2cb0f
Daniel Dogeanu <contact@danieldogeanu.com>
2024-07-28 18:44:02 +0300

    generate seo metadata dynamically for root layout

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,10 +1,11 @@
 import { ColorSchemeScript, MantineProvider } from '@mantine/core';
 import { ErrorBoundary } from 'react-error-boundary';
+import { getSiteSettings } from '@/data/settings';
 import type { Metadata } from 'next';
-import mantineTheme from '@/theme';
-import ErrorFallback from './error';
+import ErrorFallback from '@/app/error';
 import SiteHeader from '@/layout/header';
 import SiteFooter from '@/layout/footer';
+import mantineTheme from '@/theme';
 import '@mantine/core/styles.css';
 import '@/styles/global.scss';
 
@@ -14,18 +15,36 @@ interface RootLayoutProps {
 }
 
 // Default SEO Metadata
-export const metadata: Metadata = {
-  title: {
-    template: '%s > Next Story',
-    default: 'Next Story',
-  },
-  description: 'Proof of Concept project, to demonstrate the integration of the chosen tech stack to customers.',
-  robots: {
-    index: false,
-    follow: false,
-    nocache: true,
-  },
-};
+export async function generateMetadata(): Promise<Metadata> {
+  const siteSettingsResponse = await getSiteSettings({populate: '*'});
+  const siteSettings = siteSettingsResponse?.data?.attributes;
+  const defaultMetadata = {
+    title: {
+      template: '%s > Next Story',
+      default: 'Next Story',
+    },
+    description: 'Demo Website',
+    robots: {
+      index: false,
+      follow: false,
+      nocache: true,
+    },
+  };
+
+  return {
+    ...defaultMetadata,
+    title: {
+      template: `%s > ${siteSettings.siteName}`,
+      default: siteSettings.siteName,
+    },
+    description: siteSettings.siteDescription,
+    robots: {
+      index: siteSettings.indexAllowed,
+      follow: siteSettings.followAllowed,
+      nocache: !siteSettings.cacheAllowed,
+    },
+  };
+}
 
 export default function RootLayout({ children }: Readonly<RootLayoutProps>) {
   const colorScheme = 'auto';

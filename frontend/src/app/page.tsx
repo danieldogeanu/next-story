b832a1cceef43458f5cfd67ade9156b37b440e24
Daniel Dogeanu <contact@danieldogeanu.com>
2024-07-28 19:09:52 +0300

    add metadata to homepage

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,6 +1,17 @@
+import { Metadata } from 'next';
+import { getSiteSettings, SiteSettings } from '@/data/settings';
 import { getArticlesCollection } from '@/data/articles';
 import ArticleCard from '@/components/article-card';
 import styles from '@/styles/page.module.scss';
+
+export async function generateMetadata(): Promise<Metadata> {
+  const siteSettingsResponse = await getSiteSettings({populate: '*'});
+  const siteSettings = siteSettingsResponse?.data?.attributes as SiteSettings;
+
+  return {
+    title: `${siteSettings.siteName} > ${siteSettings.siteTagline}`,
+  };
+}
 
 export default async function Home() {
   const articlesCollection = await getArticlesCollection({populate: '*'});

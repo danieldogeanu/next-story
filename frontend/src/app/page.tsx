6764693710aa324133ccc0a98c78582f48518b81
Daniel Dogeanu <contact@danieldogeanu.com>
2024-08-08 02:43:46 +0300

    add open graph to homepage metadata

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,16 +1,23 @@
-import type { Metadata } from 'next';
+import type { Metadata, ResolvingMetadata } from 'next';
 import { getSiteSettings, SiteSettings } from '@/data/settings';
 import { getArticlesCollection } from '@/data/articles';
 import { makeSeoTitle } from '@/utils/client/seo';
+import { getFrontEndURL } from '@/utils/client/env';
 import ArticleCard from '@/components/article-card';
 import styles from '@/styles/page.module.scss';
 
-export async function generateMetadata(): Promise<Metadata> {
+export async function generateMetadata(props: null, parent: ResolvingMetadata): Promise<Metadata> {
+  const parentData = await parent;
   const siteSettingsResponse = await getSiteSettings({populate: '*'});
   const siteSettings = siteSettingsResponse?.data?.attributes as SiteSettings;
 
   return {
     title: makeSeoTitle(`${siteSettings?.siteName} > ${siteSettings?.siteTagline}`),
+    openGraph: {
+      ...parentData.openGraph,
+      url: getFrontEndURL(),
+      title: makeSeoTitle(siteSettings?.siteTagline),
+    }
   };
 }
 

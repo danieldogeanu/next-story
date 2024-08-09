0fd809092894bd209aaa7c93bb06210d82d8c0c8
Daniel Dogeanu <contact@danieldogeanu.com>
2024-08-09 11:36:42 +0300

    add open graph to tags page metadata

--- frontend/src/app/tags/page.tsx
+++ frontend/src/app/tags/page.tsx
@@ -1,9 +1,10 @@
 import type { Metadata, ResolvingMetadata } from 'next';
 import { Title } from '@mantine/core';
 import { getTagsCollection } from '@/data/tags';
-import { getSinglePageSettings, PageRobots } from '@/data/settings';
+import { getSinglePageSettings, PageCover, PageMetaSocial, PageMetaSocialEntry, PageRobots } from '@/data/settings';
 import { makeSeoDescription, makeSeoKeywords, makeSeoTitle } from '@/utils/client/seo';
-import { generateRobotsObject } from '@/utils/server/seo';
+import { generateCoverImageObject, generateRobotsObject } from '@/utils/server/seo';
+import { getFrontEndURL } from '@/utils/client/env';
 import { capitalize } from '@/utils/strings';
 import TagCard from '@/components/tag-card';
 import styles from '@/styles/page.module.scss';
@@ -12,12 +13,23 @@ export async function generateMetadata(props: null, parent: ResolvingMetadata): 
   const parentData = await parent;
   const tagsPageSettings = await getSinglePageSettings('tags');
   const tagsPageRobots = tagsPageSettings?.robots as PageRobots;
+  const tagsCover = tagsPageSettings?.cover?.data?.attributes as PageCover;
+  const tagsMetaSocials = tagsPageSettings?.metaSocial as PageMetaSocial;
+  const tagsMetaFacebook = tagsMetaSocials.filter((social) => (social.socialNetwork === 'Facebook')).pop() as PageMetaSocialEntry;
+  const tagsMetaFacebookImage = tagsMetaFacebook?.image?.data?.attributes as PageCover;
 
   return {
     title: makeSeoTitle(tagsPageSettings?.title, parentData.applicationName),
     description: makeSeoDescription(tagsPageSettings?.description),
     keywords: makeSeoKeywords(tagsPageSettings?.keywords),
     robots: await generateRobotsObject(tagsPageRobots),
+    openGraph: {
+      ...parentData.openGraph,
+      title: makeSeoTitle((tagsMetaFacebook?.title || tagsPageSettings?.title), parentData.applicationName),
+      description: makeSeoDescription(tagsMetaFacebook?.description || tagsPageSettings?.description, 65),
+      url: new URL((tagsPageSettings?.canonicalURL || 'tags'), getFrontEndURL()).href,
+      images: await generateCoverImageObject(tagsMetaFacebookImage || tagsCover),
+    },
   };
 }
 

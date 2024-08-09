4e64eb2a8a4564763f47329508cf104f8b92d478
Daniel Dogeanu <contact@danieldogeanu.com>
2024-08-09 11:32:10 +0300

    update tags page metadata to use seo utils

--- frontend/src/app/tags/page.tsx
+++ frontend/src/app/tags/page.tsx
@@ -1,20 +1,22 @@
-import type { Metadata } from 'next';
+import type { Metadata, ResolvingMetadata } from 'next';
 import { Title } from '@mantine/core';
 import { getTagsCollection } from '@/data/tags';
 import { getSinglePageSettings, PageRobots } from '@/data/settings';
+import { makeSeoDescription, makeSeoKeywords, makeSeoTitle } from '@/utils/client/seo';
 import { generateRobotsObject } from '@/utils/server/seo';
 import { capitalize } from '@/utils/strings';
 import TagCard from '@/components/tag-card';
 import styles from '@/styles/page.module.scss';
 
-export async function generateMetadata(): Promise<Metadata> {
+export async function generateMetadata(props: null, parent: ResolvingMetadata): Promise<Metadata> {
+  const parentData = await parent;
   const tagsPageSettings = await getSinglePageSettings('tags');
   const tagsPageRobots = tagsPageSettings?.robots as PageRobots;
 
   return {
-    title: capitalize(tagsPageSettings?.title.trim() as string),
-    description: tagsPageSettings?.description?.trim().substring(0, 160),
-    keywords: tagsPageSettings?.keywords,
+    title: makeSeoTitle(tagsPageSettings?.title, parentData.applicationName),
+    description: makeSeoDescription(tagsPageSettings?.description),
+    keywords: makeSeoKeywords(tagsPageSettings?.keywords),
     robots: await generateRobotsObject(tagsPageRobots),
   };
 }

1e14354a10b4fc4724a3b48b39938476657dd853
Daniel Dogeanu <contact@danieldogeanu.com>
2024-08-09 11:07:43 +0300

    add open graph to categories page metadata

--- frontend/src/app/categories/page.tsx
+++ frontend/src/app/categories/page.tsx
@@ -1,9 +1,10 @@
 import type { Metadata, ResolvingMetadata } from 'next';
 import { Title } from '@mantine/core';
 import { getCategoriesCollection } from '@/data/categories';
-import { getSinglePageSettings, PageRobots } from '@/data/settings';
+import { getSinglePageSettings, PageCover, PageMetaSocial, PageMetaSocialEntry, PageRobots } from '@/data/settings';
 import { makeSeoDescription, makeSeoKeywords, makeSeoTitle } from '@/utils/client/seo';
-import { generateRobotsObject } from '@/utils/server/seo';
+import { generateCoverImageObject, generateRobotsObject } from '@/utils/server/seo';
+import { getFrontEndURL } from '@/utils/client/env';
 import { capitalize } from '@/utils/strings';
 import CategoryCard from '@/components/category-card';
 import styles from '@/styles/page.module.scss';
@@ -12,12 +13,23 @@ export async function generateMetadata(props: null, parent: ResolvingMetadata): 
   const parentData = await parent;
   const categoriesPageSettings = await getSinglePageSettings('categories');
   const categoriesPageRobots = categoriesPageSettings?.robots as PageRobots;
+  const categoriesCover = categoriesPageSettings?.cover?.data?.attributes as PageCover;
+  const categoriesMetaSocials = categoriesPageSettings?.metaSocial as PageMetaSocial;
+  const categoriesMetaFacebook = categoriesMetaSocials.filter((social) => (social.socialNetwork === 'Facebook')).pop() as PageMetaSocialEntry;
+  const categoriesMetaFacebookImage = categoriesMetaFacebook?.image?.data?.attributes as PageCover;
 
   return {
     title: makeSeoTitle(categoriesPageSettings?.title, parentData.applicationName),
     description: makeSeoDescription(categoriesPageSettings?.description),
     keywords: makeSeoKeywords(categoriesPageSettings?.keywords),
     robots: await generateRobotsObject(categoriesPageRobots),
+    openGraph: {
+      ...parentData.openGraph,
+      title: makeSeoTitle((categoriesMetaFacebook?.title || categoriesPageSettings?.title), parentData.applicationName),
+      description: makeSeoDescription(categoriesMetaFacebook?.description || categoriesPageSettings?.description, 65),
+      url: new URL((categoriesPageSettings?.canonicalURL || 'categories'), getFrontEndURL()).href,
+      images: await generateCoverImageObject(categoriesMetaFacebookImage || categoriesCover),
+    },
   };
 }
 

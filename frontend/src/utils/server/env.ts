dfa2df37a61e90a476ea38baf8b2b862ce494358
Daniel Dogeanu <contact@danieldogeanu.com>
2024-08-16 11:41:30 +0300

    refactor getAPIKey function to not throw error

--- frontend/src/utils/server/env.ts
+++ frontend/src/utils/server/env.ts
@@ -20,21 +20,16 @@ export async function isBuildTime() {
 /**
  * Retrieves the API key for the specified purpose.
  *
- * `WARNING`: API keys should not be exposed to the client (browser)! Because of this we made
- * this function a server function. API keys can only be retrieved on the server.
+ * @warning API keys should not be exposed to the client (browser)!  \
+ * Because of this we made this function a server function. API keys can only be retrieved on the server.
  *
  * @param keyFor - Specifies whether the API key is for the frontend or backend.
  * @returns A promise that resolves to the API key. Returns an empty string if the key is not found.
- * @throws Throws an error if the `keyFor` parameter is not specified or is invalid.
- *
- * @example
- * // Retrieve the frontend API key
- * await getAPIKey('frontend');
  */
 export async function getAPIKey(keyFor: 'frontend' | 'backend') {
   switch(keyFor) {
     case 'frontend': return (process.env.NEXT_STRAPI_FE_APIKEY || '');
     case 'backend': return (process.env.NEXT_STRAPI_BE_APIKEY || '');
-    default: throw new Error('The API key type was either not specified or not found.');
+    default: return '';
   }
 }

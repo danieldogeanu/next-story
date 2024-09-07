40986de725c8adaa8d4112d5ffc83e23919ec4b5
Daniel Dogeanu <contact@danieldogeanu.com>
2024-09-07 04:33:23 +0300

    add single author page to refactored authors page

--- frontend/src/app/authors-refactor/[[...slug]]/page.tsx
+++ frontend/src/app/authors-refactor/[[...slug]]/page.tsx
@@ -50,7 +50,130 @@ export default async function AuthorsPage({params}: AuthorPageProps) {
   // If it's the first page, we need to redirect to avoid page duplicates.
   firstPageRedirect(slug, pageNumber, '/authors-refactor');
 
-  // If there's no slug, we're on the root `authors` page. 
+  // Single Author Page
+  // ---------------------------------------------------------------------------
+  
+  // If there's a slug, we're most likely on the Single Author page.
+  if (typeof slug === 'string') {
+
+    const authorData = (await getAuthorsCollection({
+      filters: { slug: { $eq: params.slug } },
+      populate: {
+        avatar: { populate: '*' },
+        socialNetworks: { populate: '*' },
+        articles: { populate: '*' },
+        seo: { populate: '*' },
+      },
+      pagination: { start: 0, limit: 1 },
+    })).data.pop()?.attributes as SingleAuthor;
+  
+    if (typeof authorData === 'undefined') return notFound();
+  
+    const authorAvatar = authorData?.avatar?.data?.attributes as AuthorAvatar;
+    const authorAvatarFormats = authorAvatar?.formats as unknown as StrapiImageFormats;
+    const authorAvatarUrl = (authorAvatarFormats?.small?.url) ? getFileURL(authorAvatarFormats.small.url) : null;
+    const authorSocials = authorData?.socialNetworks as unknown as AuthorSocialEntry[];
+    const authorArticles = authorData?.articles?.data as AuthorArticles;
+    const authorArticlesNumber = (authorArticles && authorArticles.length) ? authorArticles.length : 0;
+  
+    return (
+      <main className={pageStyles.main}>
+  
+        <section className={classNames(pageStyles.container, authorStyles.intro)}>
+  
+          <Title className={pageStyles.pageTitle}>
+            {capitalize(authorData?.fullName) + '\'s'} Articles
+          </Title>
+  
+          <Box className={authorStyles.author}>
+  
+            <Box className={authorStyles.left}>
+  
+              <Box className={authorStyles.avatar}>
+                {(authorAvatar && authorAvatarUrl) ?
+                  <Image
+                    className={authorStyles.image}
+                    component={NextImage}
+                    src={authorAvatarUrl}
+                    width={authorAvatarFormats.small?.width}
+                    height={authorAvatarFormats.small?.height}
+                    alt={authorAvatar?.alternativeText || ''} />
+                  :  
+                  <Box className={authorStyles.default}>
+                    <IconUser size={100} stroke={1} />
+                  </Box>
+                }
+              </Box>
+  
+              <Text className={authorStyles.joined} title='Date Joined'>
+                Joined {capitalize(convertToRelativeDate(authorData.publishedAt))}
+              </Text>
+  
+              <Group className={authorStyles.socials} justify='center'>
+                {(authorSocials && authorSocials.length > 0) && authorSocials.map((social) => (
+                  <SocialIcon
+                    key={social.id}
+                    href={social.link}
+                    label={social.label}
+                    icon={social.icon || 'IconCircleX'}
+                    className={authorStyles.icon}
+                    color='dark' />
+                ))}
+              </Group>
+  
+              <Text className={authorStyles.total} title='Total Articles'>
+                Written {authorArticlesNumber} {(authorArticlesNumber === 1) ? 'Article' : 'Articles'}
+              </Text>
+  
+            </Box>
+  
+            <Box className={authorStyles.right}>
+  
+              <Title className={authorStyles.name} order={2}>
+                {capitalize(authorData?.fullName)}
+              </Title>
+  
+              <Box className={authorStyles.biography}>
+                {authorData?.biography.split('\n\n').map((paragraph, index) => {
+                  return <Text key={index}>{paragraph.trim()}</Text>;
+                })}
+              </Box>
+  
+              <Group className={authorStyles.actions}>
+  
+                <Button
+                  className={authorStyles.subscribeButton}
+                  leftSection={<IconMailPlus size={24} stroke={1.5} />}
+                  color='blue' variant='filled' size='md'>Subscribe</Button>
+  
+                <Button
+                  className={authorStyles.sponsorButton}
+                  leftSection={<IconCoin size={24} stroke={1.5} />}
+                  variant='filled' size='md'>Sponsor</Button>
+  
+              </Group>
+  
+            </Box>
+  
+          </Box>
+  
+        </section>
+  
+        <section className={pageStyles.grid}>
+          {authorArticles?.map((article) => {
+            return <ArticleCard key={article.id} data={article.attributes} />
+          })}
+        </section>
+  
+      </main>
+    );
+
+  } // Single Author Page
+  
+  // Authors Page
+  // ---------------------------------------------------------------------------
+
+  // If there's no slug, we're on the root Authors page. 
   const authorsCollection = await getAuthorsCollection({
     populate: '*', sort: 'id:desc',
     pagination: { page: pageNumber || 1, pageSize: 12 },

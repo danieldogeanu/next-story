a71a0218d564d5f8354e062ebb10821c883de213
Daniel Dogeanu <contact@danieldogeanu.com>
2024-09-08 14:21:38 +0300

    add single tag page to refactored tags page

--- frontend/src/app/tags-refactor/[[...slug]]/page.tsx
+++ frontend/src/app/tags-refactor/[[...slug]]/page.tsx
@@ -13,6 +13,7 @@ import ArticleCard from '@/components/article-card';
 import SortBar from '@/components/sort-bar';
 import TagCard from '@/components/tag-card';
 import styles from '@/styles/page.module.scss';
+import { getArticlesCollection } from '@/data/articles';
 
 
 export interface TagsPageProps {
@@ -43,13 +44,67 @@ export default async function TagsPage({params}: TagsPageProps) {
   // If it's the first page, we need to redirect to avoid page duplicates.
   firstPageRedirect(slug, pageNumber, rootPageSlug);
 
+  // Single Tag Page
+  // ---------------------------------------------------------------------------
+  
+  // If there's a slug, we're most likely on the Single Tag page.
+  if (typeof slug === 'string') {
+
+    // Get single tag data without articles, so we can handle pagination later.
+    const tagData = (await getTagsCollection({
+      filters: { slug: { $eq: slug } },
+      pagination: { start: 0, limit: 1 },
+    })).data.pop()?.attributes as SingleTag;
+  
+    // If the tagData array is empty or undefined, it means no tag was found.
+    if (typeof tagData === 'undefined') return notFound();
+  
+    // Get all articles that belong to the current tag, and split it into pages.
+    const articlesResponse = (await getArticlesCollection({
+      populate: '*', filters: { tags: { slug: { $eq: slug } } },
+      pagination: { page: pageNumber || 1, pageSize: 24 },
+    }));
+    const articlesData = articlesResponse?.data as TagArticles;
+    const articlesPagination = articlesResponse?.meta?.pagination;
+  
+    // If the page number is beyond of the page count, we return a 404.
+    outOfBoundsRedirect(pageNumber, articlesPagination?.pageCount, articlesData?.length);
+
+    return (
+      <main className={styles.main}>
+  
+        <section className={styles.container}>
+
+          <Title className={styles.pageTitle}>
+            {capitalize(tagData?.name.trim())} Tag
+          </Title>
+
+          <Suspense fallback={null}>
+            <SortBar />
+          </Suspense>
+
+        </section>
+  
+        <section className={styles.grid}>
+          {articlesData?.map((article) => {
+            return <ArticleCard key={article.id} data={article.attributes} />;
+          })}
+        </section>
+
+        <PagePagination data={articlesPagination} />
+  
+      </main>
+    );
+
+  } // Single Tag Page
+
   // Tags Page
   // ---------------------------------------------------------------------------
   
   // If there's no slug, we're on the root Tags page. 
   const tagsCollection = await getTagsCollection({
     populate: '*', sort: 'id:desc',
-    pagination: { page: pageNumber || 1, pageSize: 4 },
+    pagination: { page: pageNumber || 1, pageSize: 24 },
   });
   const tagsPageSettings = await getSinglePageSettings('tags');
   const tagsPagination = tagsCollection?.meta?.pagination;

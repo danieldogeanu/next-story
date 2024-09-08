2e8e6e057519e345fcc3467b84bf1091b5fcb1be
Daniel Dogeanu <contact@danieldogeanu.com>
2024-09-08 07:48:47 +0300

    add root categories page with pagination enabled

--- frontend/src/app/categories-refactor/[[...slug]]/page.tsx
+++ frontend/src/app/categories-refactor/[[...slug]]/page.tsx
@@ -7,13 +7,16 @@ import {
   CategoryArticles, CategoryCover, CategoryMetaSocial, CategoryMetaSocialEntry, CategoryRobots, 
   CategorySEO, getCategoriesCollection, SingleCategory
 } from '@/data/categories';
-import { checkSlugAndRedirect, extractSlugAndPage, firstPageRedirect, getFileURL, getPageUrl } from '@/utils/urls';
+import { checkSlugAndRedirect, extractSlugAndPage, firstPageRedirect, getFileURL, getPageUrl, outOfBoundsRedirect } from '@/utils/urls';
 import { makeSeoDescription, makeSeoKeywords, makeSeoTitle } from '@/utils/client/seo';
 import { generateCoverImageObject, generateRobotsObject } from '@/utils/server/seo';
+import { getSinglePageSettings } from '@/data/settings';
 import { isSlugArrayValid } from '@/validation/urls';
 import { StrapiImageFormats } from '@/types/strapi';
 import { capitalize } from '@/utils/strings';
 import ArticleCard from '@/components/article-card';
+import CategoryCard from '@/components/category-card';
+import PagePagination from '@/components/page-pagination';
 import defaultCover from '@/assets/imgs/default-cover.webp';
 import pageStyles from '@/styles/page.module.scss';
 import categoryStyles from '@/styles/category-page.module.scss';
@@ -47,12 +50,34 @@ export default async function CategoriesPage({params}: CategoriesPageProps) {
   // If it's the first page, we need to redirect to avoid page duplicates.
   firstPageRedirect(slug, pageNumber, rootPageSlug);
   
+  // Categories Page
+  // -----------------------------------------------------------------------------
+  
+  // If there's no slug, we're on the root Categories page.
+  const categoriesCollection = await getCategoriesCollection({
+    populate: '*', sort: 'id:desc',
+    pagination: { page: pageNumber || 1, pageSize: 12 },
+  });
+  const categoriesPageSettings = await getSinglePageSettings('categories');
+  const categoriesPagination = categoriesCollection?.meta?.pagination;
+
+  // If the page number is beyond of the page count, we return a 404.
+  outOfBoundsRedirect(pageNumber, categoriesPagination?.pageCount, categoriesCollection?.data?.length);
+
   return (
     <main className={pageStyles.main}>
 
       <Title className={pageStyles.pageTitle}>
-        Categories Refactor
+        {capitalize(categoriesPageSettings?.title.trim() || 'Categories')}
       </Title>
+
+      <section className={pageStyles.grid}>
+        {categoriesCollection.data.map((category) => {
+          return (<CategoryCard key={category.id} data={category.attributes} />);
+        })}
+      </section>
+
+      <PagePagination data={categoriesPagination} />
 
     </main>
   );

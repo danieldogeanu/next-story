3b69bb6a71511dbd67e755261cf5129b674585f5
Daniel Dogeanu <contact@danieldogeanu.com>
2024-09-08 13:45:48 +0300

    add slug array validation and redirection for refactored tags page

--- frontend/src/app/tags-refactor/[[...slug]]/page.tsx
+++ frontend/src/app/tags-refactor/[[...slug]]/page.tsx
@@ -3,8 +3,9 @@ import { Suspense } from 'react';
 import { notFound } from 'next/navigation';
 import { Title } from '@mantine/core';
 import { getTagsCollection, SingleTag, TagArticles } from '@/data/tags';
+import { checkSlugAndRedirect, extractSlugAndPage, firstPageRedirect, getPageUrl } from '@/utils/urls';
 import { makeSeoKeywords, makeSeoTitle } from '@/utils/client/seo';
-import { getPageUrl } from '@/utils/urls';
+import { isSlugArrayValid } from '@/validation/urls';
 import ArticleCard from '@/components/article-card';
 import SortBar from '@/components/sort-bar';
 import styles from '@/styles/page.module.scss';
@@ -19,11 +20,24 @@ export interface TagsPageProps {
 const rootPageSlug = '/tags-refactor';
 
 export async function generateMetadata({params}: TagsPageProps, parent: ResolvingMetadata): Promise<Metadata> {
+  if (!isSlugArrayValid(params.slug)) return {};
+  const {slug, pageNumber} = extractSlugAndPage(params.slug);
+
+  const parentData = await parent;
   
   return {};
 }
 
 export default async function TagsPage({params}: TagsPageProps) {
+  // Check if the slug array is a valid path and if not, return a 404.
+  // If the slug array contains a `page` keyword, but no page number, redirect to the slug, or root page.
+  checkSlugAndRedirect(params.slug, rootPageSlug);
+
+  // If the slug array is valid, proceed to extract the slug and page number if they're present.
+  const {slug, pageNumber} = extractSlugAndPage(params.slug);
+
+  // If it's the first page, we need to redirect to avoid page duplicates.
+  firstPageRedirect(slug, pageNumber, rootPageSlug);
   
   return (
     <main className={styles.main}>

a43ae94683ac2308be305be4fef3c51b80edce1d
Daniel Dogeanu <contact@danieldogeanu.com>
2024-09-10 22:39:57 +0300

    replace first page & out of bounds redirects with urls utils functions

--- frontend/src/app/page/[number]/page.tsx
+++ frontend/src/app/page/[number]/page.tsx
@@ -1,10 +1,9 @@
 import type { Metadata, ResolvingMetadata } from 'next';
-import { notFound, permanentRedirect, RedirectType } from 'next/navigation';
+import { notFound } from 'next/navigation';
 import { getSiteSettings, SiteSettings } from '@/data/settings';
 import { getArticlesCollection } from '@/data/articles';
 import { addPageNumber, makeSeoTitle } from '@/utils/client/seo';
-import { getFrontEndURL } from '@/utils/client/env';
-import { getPageUrl } from '@/utils/urls';
+import { firstPageRedirect, getPageUrl, outOfBoundsRedirect } from '@/utils/urls';
 import ArticleCard from '@/components/article-card';
 import PagePagination from '@/components/page-pagination';
 import styles from '@/styles/page.module.scss';
@@ -45,16 +44,16 @@ export default async function Home({params}: Readonly<HomeProps>) {
   if (isNaN(Number(params.number))) return notFound();
 
   // If it's the first page, we need to redirect to avoid page duplicates.
-  if (Number(params.number) === 1) permanentRedirect(getFrontEndURL(), RedirectType.replace);
+  firstPageRedirect(null, Number(params.number), null);
 
   const articlesCollection = await getArticlesCollection({
     populate: '*', sort: 'id:desc',
     pagination: { page: Number(params.number), pageSize: 24 },
   });
   const articlesPagination = articlesCollection?.meta?.pagination;
-  const isOutOfBounds = Number(params.number) > Number(articlesPagination.pageCount);
-
-  if (articlesCollection.data.length === 0 && isOutOfBounds) return notFound();
+  
+  // If the page number is beyond of the page count, we return a 404.
+  outOfBoundsRedirect(Number(params.number), articlesPagination.pageCount, articlesCollection.data.length);
 
   return (
     <main className={styles.main}>

2cc847782734e9705f46f83942523822055264ab
Daniel Dogeanu <contact@danieldogeanu.com>
2024-09-12 04:30:17 +0300

    add slug array validation and redirection for refactored homepage

--- frontend/src/app/[[...slug]]/page.tsx
+++ frontend/src/app/[[...slug]]/page.tsx
@@ -2,12 +2,13 @@ import NextImage from 'next/image';
 import type { Metadata, ResolvingMetadata } from 'next';
 import { notFound, permanentRedirect, RedirectType } from 'next/navigation';
 import { Box, Image, Title } from '@mantine/core';
-import { getPagesCollection, PageContent, PageCover, PageMetaSocialEntry, PageMetaSocial, PageRobots, PageSEO, SinglePage } from '@/data/pages';
-import { getSiteSettings, SiteSettings } from '@/data/settings';
 import { getArticlesCollection } from '@/data/articles';
+import { getSiteSettings, SiteSettings } from '@/data/settings';
+import { getPagesCollection, PageContent, PageCover, PageMetaSocialEntry, PageMetaSocial, PageRobots, PageSEO, SinglePage } from '@/data/pages';
+import { getPageUrl, getFileURL, checkSlugAndRedirect, extractSlugAndPage, firstPageRedirect } from '@/utils/urls';
 import { makeSeoDescription, makeSeoKeywords, makeSeoTitle } from '@/utils/client/seo';
 import { generateCoverImageObject, generateRobotsObject } from '@/utils/server/seo';
-import { getPageUrl, getFileURL } from '@/utils/urls';
+import { isSlugArrayValid } from '@/validation/urls';
 import { getFrontEndURL } from '@/utils/client/env';
 import { StrapiImageFormats } from '@/types/strapi';
 import ArticleCard from '@/components/article-card';
@@ -23,14 +24,29 @@ export interface PageProps {
   };
 }
 
-export async function generateMetadata({params}: PageProps, parent: ResolvingMetadata): Promise<Metadata> {
+const rootPageSlug = '/';
 
+export async function generateMetadata({params}: PageProps, parent: ResolvingMetadata): Promise<Metadata> {
+  if (!isSlugArrayValid(params.slug)) return {};
+  const {slug, pageNumber} = extractSlugAndPage(params.slug);
+  
+  const parentData = await parent;
+  
   return {};
 }
 
 
 export default async function Page({params}: PageProps) {
-  
+  // Check if the slug array is a valid path and if not, return a 404.
+  // If the slug array contains a `page` keyword, but no page number, redirect to the slug, or root page.
+  checkSlugAndRedirect(params.slug, rootPageSlug);
+
+  // If the slug array is valid, proceed to extract the slug and page number if they're present.
+  const {slug, pageNumber} = extractSlugAndPage(params.slug);
+
+  // If it's the first page, we need to redirect to avoid page duplicates.
+  firstPageRedirect(slug, pageNumber, rootPageSlug);
+
   return (
     <main className={styles.main}>
 

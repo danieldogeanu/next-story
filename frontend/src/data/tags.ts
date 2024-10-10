751cc1fba71cbd8b8407ef797c9d7963027d6235
Daniel Dogeanu <contact@danieldogeanu.com>
2024-10-10 21:09:23 +0300

    add empty response & try-catch block for tags data

--- frontend/src/data/tags.ts
+++ frontend/src/data/tags.ts
@@ -1,8 +1,7 @@
 import { StrapiRequestParams } from 'strapi-sdk-js';
 import { APIResponse, APIResponseCollection, APIResponseData, GetValues } from '@/types/strapi';
 import { getAPIKey, isBuildTime } from '@/utils/server/env';
-import { strapiSDK } from '@/data/strapi';
-import buildTimeTags from '@build-data/tags.json';
+import { emptyStrapiResponse, strapiSDK } from '@/data/strapi';
 
 // Rename Strapi types to make it more clear what we're working with.
 export interface SingleTag extends GetValues<'api::tag.tag'> {}
@@ -16,12 +15,11 @@ export type TagArticles = NonNullable<SingleTag['articles']>['data'];
 /**
  * Fetches a single tag from the Strapi backend by their ID.
  *
- * At build time, it will return a single tag from a static JSON file.
+ * At build time, it will return an empty response.
  *
  * @param {string | number} id - The ID of the tag to fetch.
  * @param {StrapiRequestParams} [params] - Optional parameters for the request.
  * @returns A promise that resolves to the tag data.
- * @throws {Error} Throws an error if the fetch request fails.
  *
  * @example
  * // Fetch a single tag by ID.
@@ -32,27 +30,28 @@ export type TagArticles = NonNullable<SingleTag['articles']>['data'];
  * await getSingleTag(1, {fields: ['name', 'slug']});
  */
 export async function getSingleTag(id: string | number, params?: StrapiRequestParams): Promise<SingleTagResponse> {
-  // At build time we load a static JSON file generated from fetcher container,
-  // because we don't have networking available to make requests directly to Strapi backend.
-  // We ignore all optional parameters for this one, as we already populated all the fields.
-  if (await isBuildTime()) return buildTimeTags.data.filter(
-    (tag) => (tag.id === Number(id))
-  ).pop() as unknown as SingleTagResponse;
+  try {
+    // At build time we return an empty response, because we don't have
+    // networking available to make requests directly to Strapi backend.
+    if (await isBuildTime()) return emptyStrapiResponse.api.single as unknown as SingleTagResponse;
 
-  // Otherwise we just make the requests to the live Strapi backend.
-  const strapiInstance = await strapiSDK(await getAPIKey('frontend'));
-  const strapiResponse = await strapiInstance.findOne('tags', id, params) as SingleTagResponse;
-  return strapiResponse;
+    // Otherwise we just make the requests to the live Strapi backend.
+    const strapiInstance = await strapiSDK(await getAPIKey('frontend'));
+    const strapiResponse = await strapiInstance.findOne('tags', id, params) as SingleTagResponse;
+    return strapiResponse;
+  } catch (e) {
+    console.error('Error:', (e instanceof Error) ? e.message : e);
+    return emptyStrapiResponse.api.single as unknown as SingleTagResponse;
+  }
 }
 
 /**
  * Fetches a collection of tags from the Strapi backend.
  *
- * At build time, it will return a collection of tags from a static JSON file.
+ * At build time, it will return an empty response.
  *
  * @param {StrapiRequestParams} [params] - Optional parameters for the request.
  * @returns A promise that resolves to a collection of tags.
- * @throws {Error} Throws an error if the fetch request fails.
  *
  * @example
  * // Fetch a collection of tags.
@@ -63,13 +62,17 @@ export async function getSingleTag(id: string | number, params?: StrapiRequestPa
  * await getTagsCollection({pagination: {page: 1, pageSize: 2}});
  */
 export async function getTagsCollection(params?: StrapiRequestParams): Promise<TagsCollectionResponse> {
-  // At build time we load a static JSON file generated from fetcher container,
-  // because we don't have networking available to make requests directly to Strapi backend.
-  // We ignore all optional parameters for this one, as we already populated all the fields.
-  if (await isBuildTime()) return buildTimeTags as unknown as TagsCollectionResponse;
+  try {
+    // At build time we return an empty response, because we don't have
+    // networking available to make requests directly to Strapi backend.
+    if (await isBuildTime()) return emptyStrapiResponse.api.collection as unknown as TagsCollectionResponse;
 
-  // Otherwise we just make the requests to the live Strapi backend.
-  const strapiInstance = await strapiSDK(await getAPIKey('frontend'));
-  const strapiResponse = await strapiInstance.find('tags', params) as unknown as TagsCollectionResponse;
-  return strapiResponse;
+    // Otherwise we just make the requests to the live Strapi backend.
+    const strapiInstance = await strapiSDK(await getAPIKey('frontend'));
+    const strapiResponse = await strapiInstance.find('tags', params) as unknown as TagsCollectionResponse;
+    return strapiResponse;
+  } catch (e) {
+    console.error('Error:', (e instanceof Error) ? e.message : e);
+    return emptyStrapiResponse.api.collection as unknown as TagsCollectionResponse;
+  }
 }

cd1f916618620faf179d33faa2a7c355809fcfd8
Daniel Dogeanu <contact@danieldogeanu.com>
2024-10-16 19:33:23 +0300

    fix undefined error in socialnav component

--- frontend/src/components/socialnav.tsx
+++ frontend/src/components/socialnav.tsx
@@ -5,7 +5,7 @@ import styles from '@/styles/socialnav.module.scss';
 export default async function SocialNav() {
   // Get site settings from Strapi and map social networks to SocialIconProps[].
   const siteSettingsData = await getSiteSettings({populate: ['socialNetworks'], fields: ['siteTitle']});
-  const socialNetworksData = siteSettingsData.data.attributes.socialNetworks;
+  const socialNetworksData = siteSettingsData?.data?.attributes?.socialNetworks;
   const socialNavEntries = socialNetworksData?.map((item) => ({
     href: item.link, label: item.label, icon: item.icon,
   })) as SocialIconProps[];

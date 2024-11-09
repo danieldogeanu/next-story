275f24b81b7eb284f7e1b38f603afcb738336c3d
Daniel Dogeanu <contact@danieldogeanu.com>
2024-11-09 05:06:20 +0200

    add event tracking to analytics

--- frontend/src/components/analytics.tsx
+++ frontend/src/components/analytics.tsx
@@ -7,6 +7,36 @@ export interface AnalyticsProps {
 }
 
 export default function Analytics({id}: AnalyticsProps) {
+  const trackEvents = () => {
+    if (typeof window !== 'undefined' && typeof window.umami === 'object') {
+      const allLinks = document.querySelectorAll('a');
+      const allButtons = document.querySelectorAll('button');
+
+      // Set event listeners for all links on the page, and attach event trackers to them.
+      allLinks.forEach((link) => link.addEventListener('click', (e) => {
+        const currentLink = e.currentTarget as HTMLAnchorElement;
+        if (currentLink.hasAttribute('title')) {
+          umami.track(`Link Click: ${currentLink.title}`, { href: currentLink.href });
+        } else if (currentLink.hasAttribute('ariaLabel')) {
+          umami.track(`Link Click: ${currentLink.ariaLabel}`, { href: currentLink.href });
+        } else {
+          umami.track(`Link Click: ${currentLink?.innerText || 'Unknown'}`, { href: currentLink.href });
+        }
+      }));
+
+      // Set event listeners for all buttons on the page, and attach event trackers to them.
+      allButtons.forEach((button) => button.addEventListener('click', (e) => {
+        const currentButton = e.currentTarget as HTMLButtonElement;
+        if (currentButton.hasAttribute('title')) {
+          umami.track(`Button Click: ${currentButton.title}`);
+        } else if (currentButton.hasAttribute('ariaLabel')) {
+          umami.track(`Button Click: ${currentButton.ariaLabel}`);
+        } else {
+          umami.track(`Button Click: ${currentButton?.innerText || 'Unknown'}`);
+        }
+      }));
+    }
+  };
 
   return (
     <>
@@ -15,6 +45,7 @@ export default function Analytics({id}: AnalyticsProps) {
         strategy='afterInteractive'
         src='https://umami.ddsv.eu/script.js'
         data-website-id={id}
+        onReady={trackEvents}
         async
       />
     </>
